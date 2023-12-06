bac0377c5297b3887b0b6c5530bb4dd1074dfd27
Xiaoxuan Hu <leonahu0720@gmail.com>
2023-12-05 23:12:11 -0800

    Djangou-final

--- myapp/views.py
+++ myapp/views.py
@@ -26,3 +26,36 @@ def delete_contact(request, contact_id):
     contact = Contact.objects.get(id=contact_id)
     contact.delete()
     return redirect('contact_list')
+from django.shortcuts import render, get_object_or_404
+from .models import Contact
+
+def contact_detail(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    return render(request, 'contact_detail.html', {'contact': contact})
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Contact
+from .form import ContactForm
+
+def edit_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_detail', contact_id=contact.id)
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'edit_contact.html', {'form': form})
+from django.shortcuts import redirect, get_object_or_404
+from .models import Contact
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    contact.delete()
+    return redirect('contact_list')  # Redirect to the list of contacts
+def delete_confirm(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'delete_confirm.html', {'contact': contact})
--- todo_site/urls.py
+++ todo_site/urls.py
@@ -43,3 +43,28 @@ urlpatterns = [
     path('contact/<int:contact_id>/', views.contact_detail, name='contact_detail'),
     path('delete/<int:contact_id>/', views.delete_contact, name='delete_contact'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ... other URL patterns ...
+    path('contact/<int:contact_id>/', views.contact_detail, name='contact_detail'),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ... other URL patterns ...
+    path('edit/<int:contact_id>/', views.edit_contact, name='edit_contact'),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ... other URL patterns ...
+    path('delete/<int:contact_id>/', views.delete_contact, name='delete_contact'),
+]
+urlpatterns = [
+    # ... other URL patterns ...
+    path('delete-confirm/<int:contact_id>/', views.delete_confirm, name='delete_confirm'),
+]
