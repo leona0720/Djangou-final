6b7c549dc402925be45b91a8f9ba49c514012df5
Xiaoxuan Hu <leonahu0720@gmail.com>
2023-12-07 23:33:22 -0800

    Djangou-final

--- myapp/form.py
+++ myapp/form.py
@@ -5,3 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'phone']  # Add other fields as necessary
+class ContactForm(forms.ModelForm):
+    message = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'message']
